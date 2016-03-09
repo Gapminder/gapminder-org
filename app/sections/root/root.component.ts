e8f23fcaf9da91ced17f58fbfca30e7012e149e1
georgii.rychko <georgii.rychko@valor-software.com>
2016-03-09 19:26:45 +0200

    feat(main page) - add initial latest blog post component

--- app/sections/root/root.component.ts
+++ app/sections/root/root.component.ts
@@ -1,10 +1,12 @@
 
 import {Component} from 'angular2/core';
+import {PostsList} from '../posts/posts-list.component';
 
 @Component({
   template: <string> require('./root.component.html'),
   styles: [
     <string> require('./root.component.scss')
-  ]
+  ],
+  directives: [PostsList]
 })
 export class Root {}
