08164c1e0e6e7a68a52e2ff4797e94b7e0ab2405
natali sol <natali.abyss@gmail.com>
2016-03-29 13:15:07 +0300

    feat(line soial): component line social

---
+++ app/shared/components/line-social/line-social.component.ts
@@ -0,0 +1,13 @@
+import {Component} from 'angular2/core';
+
+@Component({
+  selector: 'line-social',
+  template: <string> require('./line-social.html'),
+  styles: [
+    <string> require('./line-social.styl')
+  ]
+})
+
+export class LineSocial {
+
+}
