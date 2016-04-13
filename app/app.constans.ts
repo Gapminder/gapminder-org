d86706e0c55dba63b7d617c745ffeaef28921bcd
georgii.rychko <georgii.rychko@valor-software.com>
2016-04-13 17:53:38 +0300

    feat(EmbeddedEntry): allows to embed various types of content on the page via iframe

--- app/app.constans.ts
+++ app/app.constans.ts
@@ -1,13 +1,11 @@
 
 export module ContentfulConfig {
-  export const CONTENTFUL_ABOUT_SLUG = 'about-gapminder';
-  export const BLOGS_TYPE_NAME = 'blogpost';
   export const CONTENTFUL_NODE_PAGE_TYPE_ID = '2v88CEhdowQwccKISkCg4O';
   export const CONTENTFUL_PAGE_TREE_FOOTER_ID = 'chtZ3rOZTUc464UcooG4I';
   export const CONTENTFUL_PAGE_TREE_ROOT_ID = '3f1HYiL4oMSkcOeoWAc4wM';
-  export const VIDEOS_TYPE_NAME = 'video';
-  export const ABOUT_CONTENT_ID = '2ZUgCAScukQoeauWuOgow';
+
   export const VIDEO_CONTENT_ID = '5440rnXoCse2GG2SsKm8Iq';
   export const HTML_CONTENT_ID = 'KvvtQizggoowIY6q88a4o';
   export const IMAGE_CONTENT_ID = '7wteDzSkQoUigMaswCGaQq';
+  export const EMBEDDED_CONTENT_ID = 'embedded';
 }
