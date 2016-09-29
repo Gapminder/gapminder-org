73f658c6552d7954cb340c8bd2c6323554085469
natali sol <natali.abyss@gmail.com>
2016-09-29 09:55:38 +0300

    feat(rss): create rss generation

--- server.js
+++ server.js
@@ -5,10 +5,14 @@ const path = require('path');
 // const favicon = require('serve-favicon');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-
+const _ = require('lodash');
 const ROOT = path.join(__dirname, 'dist');
 
 const app = express();
+const rss = require('rss');
+
+const Datastore = require('nedb');
+const db = new Datastore({filename: path.join(__dirname, '/db'), autoload: true});
 
 // prerender
 /*eslint-disable*/
@@ -18,12 +22,81 @@ if (!process.env.PRERENDER_TOKEN) {
 app.use(require('prerender-node').set('prerenderToken', process.env.PRERENDER_TOKEN));
 /*eslint-enable*/
 
-app.use(bodyParser.json());
+app.use(bodyParser.json({type: 'application/*'}));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(ROOT));
 
-app.use('*', (req, res) => res.sendFile(path.resolve(ROOT, 'index.html')));
+/*eslint-disable*/
+const feed = new rss({
+  title: 'Gapminder RSS Feed',
+  description: 'Gapminder.org RSS Feed',
+  author: 'Gapminder.org',
+  site_url: 'https://www.gapminder.org/',
+  image_url: 'http://www.gapminder.org/public/logo.png'
+});
+/*eslint-enable*/
+
+/*eslint-disable*/
+app.post('/rss', (req, res) => {
+  feed.feed_url = `https://${req.headers.host}`;
+  feed.site_url = `https://${req.headers.host}`;
+  if (req.body.sys.contentType.sys.id === 'article') {
+
+    const itemTemplate = {
+      title: req.body.fields.title['en-US'],
+      description: req.body.fields.description ? req.body.fields.description['en-US'] : '',
+      url: `https://${req.headers.host}/${req.body.fields.slug['en-US']}`,
+      guid: req.body.fields.slug['en-US'],
+      categories: [],
+      author: 'gapminder',
+      date: req.body.fields.createdAt,
+      enclosure: false,
+      custom_elements: []
+    };
+    db.findOne({guid: req.body.fields.slug['en-US']}, function (err, item) {
+      if (!_.isEmpty(item)) {
+        db.remove(item, {multi: true}, function (err) {
+          if (err) {
+            console.error('Error: ', err)
+          }
+        });
+        dbInsert(itemTemplate)
+      } else {
+        dbInsert(itemTemplate);
+      }
+    });
+    res.status(200).json({
+      message: 'ok got it!'
+    });
+  }
+  function dbInsert(itemTemplate) {
+    db.insert(itemTemplate, function (err) {
+      if (err) {
+        console.error('Error: ', err)
+      }
+    })
+  }
+});
+
+app.get('/feed/rss', (req, res) => {
+  res.type('rss');
+  res.set('Content-Type', 'text/xml');
+  feed.items = [];
+  db.find({}, (err, items) => {
+    if (err) {
+      console.error('Error: ', err)
+    }
+    if (!_.isEmpty(items)) {
+      items.forEach(product => {
+        feed.item(product);
+      });
+    }
+    res.send(feed.xml());
+  });
+});
+/*eslint-enable*/
+app.get('*', (req, res) => res.sendFile(path.resolve(ROOT, 'index.html')));
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
@@ -32,7 +105,6 @@ app.use((req, res, next) => {
   err.status = 404;
   next(err);
 });
-
 /*eslint-disable*/
 const port = process.env.PORT || 8081;
 
